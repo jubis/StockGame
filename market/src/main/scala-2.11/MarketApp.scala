0a3582c141961b49023bf3e5992ef650cc797a74
Klaus Julin <klaus.julin@gmail.com>
2016-01-30 22:30:22 +0200

    Refactors bank to more asynchonous

--- market/src/main/scala-2.11/MarketApp.scala
+++ market/src/main/scala-2.11/MarketApp.scala
@@ -1,8 +1,8 @@
-import akka.actor.{Actor, ActorSystem, Props}
+import akka.actor.{Status, Actor, ActorSystem, Props}
 import fi.hackathon.api._
 import play.api.libs.json.Json
 
-import scala.util.Try
+import scala.util.{Failure, Success, Try}
 
 object MarketApp extends App {
   implicit val system = ActorSystem("MarketSystem")
@@ -12,19 +12,31 @@ object MarketApp extends App {
 class MarketActor extends Actor {
 
   override def receive: Receive = {
-    case msg: GetCurrentPrice => sender ! getCurrentPrice(msg.symbol)
+    case msg: GetCurrentPrice => {
+
+      println(s"Request for price of ${msg.symbol}")
+
+      val price = getCurrentPrice(msg.symbol)
+
+      println(s"Price $price")
+
+      price match {
+        case Success(price) => sender ! price
+        case Failure(e) => sender ! Status.Failure(e)
+      }
+    }
   }
 
   def yahooQuery(symbol: String): String = s"https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%3D%22$symbol%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
 
-  def getCurrentPrice(symbol: String): Option[CurrentPrice] = {
+  def getCurrentPrice(symbol: String): Try[CurrentPrice] = {
     Try {
       val url = yahooQuery(symbol)
       val jsonStr = scala.io.Source.fromURL(url).mkString
       val json = Json.parse(jsonStr)
       val ask = (json \ "query" \ "results" \ "quote" \ "Ask").as[String]
       val bid = (json \ "query" \ "results" \ "quote" \ "Bid").as[String]
-      Some(CurrentPrice(symbol, BigDecimal(ask), BigDecimal(bid)))
-    }.getOrElse(None)
+      CurrentPrice(symbol, BigDecimal(ask), BigDecimal(bid))
+    }
   }
 }
